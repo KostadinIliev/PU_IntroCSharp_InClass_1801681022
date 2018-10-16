d6fd0a7ae8e6d92dbd222cb84d2272f76e0d5552
galyastoyanova <kokito_1999@abv.bg>
2018-10-16 15:42:05 +0300

    IntroInClass

--- W3InClass/MusicFactory/Program.cs
+++ W3InClass/MusicFactory/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Media;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -10,7 +11,35 @@ namespace MusicFactory
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Play some music");
+            Console.WriteLine("Do you to play some music?");
+            string answer =Console.ReadLine();
+
+            if (answer == "Yes")
+            {
+                // Create new SoundPlayer in the using statement.
+                using (SoundPlayer player = new SoundPlayer(@"C:\Users\fmi\Desktop\W3\PU_IntroCSharp_InClass_1801681022\W3InClass\MusicFactory\Music\Eminem - Venom.mp3"))
+                {
+                    player.PlaySync();
+                }
+
+            }
+            else if (answer == "No")
+            {
+                Console.WriteLine("Goodbye!");
+            }
+            else if (answer == "Fuck")
+            {
+                Console.WriteLine("Yes, fuck you too!!!");
+            }
+            else
+            {
+                Console.WriteLine("Wrong input!");
+            }
+
+            
+
+
+            
         }
     }
 }
