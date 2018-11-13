e3435c5863bdc739f08c621f1fb22e974fb0a4f7
galyastoyanova <kokito_1999@abv.bg>
2018-11-13 16:14:02 +0200

    InClass

---
+++ PuIntroCSharpInClass/AnastasBuyTank/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace AnastasBuyTank
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int days = 30;
+            int saving = 2; //lv
+            int throwAwayForFalseVodka = 5; //lv
+            int monthsInYear = 12;
+            double result = 0;
+            try
+            {
+                int tankPrice = int.Parse(Console.ReadLine());
+                int partyDays = int.Parse(Console.ReadLine());
+
+                int daysWithoutVodka = days - partyDays;
+                int totalMonthSaving = daysWithoutVodka * saving;
+                int netSaving = totalMonthSaving - (throwAwayForFalseVodka * partyDays);
+
+
+                double monthsForSaving4Tank = tankPrice / netSaving;
+                result = monthsForSaving4Tank / monthsInYear;
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+                Console.WriteLine("Please, try again later! ");
+            }
+            if (result < 0) Console.WriteLine("Never!");
+
+            else
+            {
+                result = Math.Round(result, 1, MidpointRounding.AwayFromZero);
+                Console.WriteLine($"{result} years");
+                string months = result.ToString().Split(',')[1];
+                Console.WriteLine($"{(int)result} years {months} months");
+            }
+           
+        }
+    }
+}
