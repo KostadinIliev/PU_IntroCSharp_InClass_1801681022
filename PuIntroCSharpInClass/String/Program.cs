a6f05d07c2d2701b819067d781fd74dd79b9f137
galyastoyanova <kokito_1999@abv.bg>
2018-11-27 15:45:18 +0200

    String in  class!

---
+++ PuIntroCSharpInClass/String/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Text;
+
+namespace String
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.OutputEncoding = Encoding.UTF8;
+            string[] names = new string[]
+            {
+                "Зайко Байко" ,
+                "Фики",
+                "Буги Барабата"
+            };
+
+            string[] names2 = new string[50];
+
+            names2[0] = "Ne e validno setvane";
+
+            for (int i = 1; i <names2.Length; i++)
+            {
+                Random rnd = new Random();
+                string fakeName = string.Empty;
+                for (int j = 0; j < 5; j++)
+                {
+                    int currNumb = rnd.Next(160, 260);
+                    char letter = (char)currNumb;
+                    fakeName = string.Concat(fakeName , letter);
+                }
+                names2[i] = fakeName;
+            }
+            int loops = names2.Length;
+            while (loops > 0)
+            {
+                if (loops == 0)
+                {
+                    break;
+                }
+                try
+                {
+                    Console.WriteLine(names2[loops]);
+                }
+                catch (Exception ex)
+                {
+
+                    Console.WriteLine(ex.Message);
+                }
+
+                
+             
+                loops--;
+            }
+
+        
+
+        }
+    }
+}
